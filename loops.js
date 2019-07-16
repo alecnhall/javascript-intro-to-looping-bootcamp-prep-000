57ab1cea785ff8bbacae764479bea239d153458d
Alec Hall <alecnhall@gmail.com>
2019-07-16 23:50:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,10 +25,10 @@ for ([initialization];[condition];[iteration]){
 function forLoop(array){
   for(let i = 0; i < 25; i ++){
     if (i = 1){
-      push.array('I am 1 strange loop.');
+      array.push('I am 1 strange loop.');
     }
     else {
-      push.array(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
 }
