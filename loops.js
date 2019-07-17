fe3b2722eb3503a8ae93511ce4e2bd6dec57b591
Alec Hall <alecnhall@gmail.com>
2019-07-17 00:21:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ for ([initialization];[condition];[iteration]){
 
 function forLoop(array){
   for(let i = 0; i < 25; i ++){
-    if (i = 1){
+    if (i == 1){
       array.push('I am 1 strange loop.');
     }
     else {
